08fafeddd0cc2e811f3ed19b9f76fa66ce8e3781
binotto <ekm7p8j@gmail.com>
2024-03-07 00:01:41 +0100

    parser for html done

--- parser.cpp
+++ parser.cpp
@@ -1,16 +1,6 @@
 #include "dom.hpp"
 #include <iostream>
 
-/* <html>
-    <body>
-        <h1>Title</h1>
-        <div id="main" class="test">
-            <p>Hello <em>world</em>!</p>
-        </div>
-    </body>
-</html>
-*/
-
 int is_valid_value(const string &value) {
   // Check if value has at least three chars
   if (value.size() < 3) {
@@ -52,8 +42,8 @@ ElementNode *read_element(string &input, int &pos) {
 
   pos++;
 
-  if (' ' == input[pos]) {
-    throw runtime_error("No whitespace allowed after <");
+  if (isspace(input[pos])) {
+    throw runtime_error("No space allowed after <");
   }
 
   string tag = "";
@@ -62,16 +52,14 @@ ElementNode *read_element(string &input, int &pos) {
   while (pos < input.size()) {
     if ('>' == input[pos]) {
       // Only found a single tag without any attributes
-      pos++;
-      if (tag.find('/') == 0)
-      {
+      if (tag.find('/') == 0) {
         tag = tag.substr(1);
       }
-      
+      pos++;
       return new (malloc(sizeof(ElementNode))) ElementNode(tag);
     }
 
-    if (' ' == input[pos]) {
+    if (isspace(input[pos])) {
       // Found tag so break to get attributes
       pos++;
       break;
@@ -84,10 +72,9 @@ ElementNode *read_element(string &input, int &pos) {
     throw runtime_error("Invalid html element");
   }
 
-  if (tag.find('/') == 0)
-      {
-        tag = tag.substr(1);
-      }
+  if (tag.find('/') == 0) {
+    tag = tag.substr(1);
+  }
 
   // Try to find attributes
   AttributeMap map;
@@ -99,10 +86,10 @@ ElementNode *read_element(string &input, int &pos) {
         map.insert(key_value);
       }
       pos++;
-      return new (malloc(sizeof(ElementNode))) ElementNode(tag);
+      return new (malloc(sizeof(ElementNode))) ElementNode(tag, map);
     }
 
-    if (' ' != input[pos]) {
+    if (!isspace(input[pos])) {
       attr += input[pos];
     } else {
       if (!attr.empty()) {
@@ -116,27 +103,57 @@ ElementNode *read_element(string &input, int &pos) {
   throw runtime_error("Invalid html element");
 }
 
-NodeBase *parse(string &input, ElementNode* root, int pos, string &text) {
-   ElementNode* node = read_element(input, pos);
-   if (node->tag == root->tag)
-   {
-    node->~ElementNode();
-    free(node);
-    return root;
-   }
-
-   root->add_child(node);
-
-   return parse(input, node, pos, text);
+int only_space_or_newline(string &str) {
+  for (char c : str) {
+    if (!isspace(c)) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
-int main(void) {
-  string value = "<html><body></body></html>";
+NodeBase *parse(string &input, ElementNode *root, int &pos, string &text,
+                vector<string> &tags) {
+  while (pos < input.size() && input[pos] != '<') {
+    text += input[pos];
+    pos++;
+  }
+
+  if (!text.empty() && !only_space_or_newline(text)) {
+    root->add_child(new (malloc(sizeof(TextNode))) TextNode(text));
+  }
+  text = "";
+
+  ElementNode *node = read_element(input, pos);
+
+  if (node->tag == root->tag) {
+    node->~ElementNode();
+    free(node);
+    tags.pop_back();
+    return root;
+  }
+  tags.push_back(node->tag);
+
+  ElementNode *child = (ElementNode *)parse(input, node, pos, text, tags);
+  root->add_child(child);
+
+  if (tags.size() != 0) {
+    return parse(input, root, pos, text, tags);
+  }
+  return root;
+}
+
+/*int main(void) {
+  string value =
+      "<html\n        >\n\n\n<body>                 \n      <h1>Title</h1><div "
+      "id=\"main\"      test=\"test1\"       \n  \n\n  "
+      "\nclass=\"test\"><p>Hello "
+      "<em>world<test>344<a></a></test></em>!</p></div></body></html>";
   vector<string> tags;
   int pos = 0;
   ElementNode root("");
   string text = "";
-  NodeBase *res = parse(value, &root, pos, text);
-  NodeBase::print(&root);
+  NodeBase *res = parse(value, &root, pos, text, tags);
+  NodeBase::print(root.children[0]);
   return 0;
-}
+}*/
